4dda478f7965f85ce97409c72982858b7f6161b2
lilian <lilian.wangechi@student.moringaschool.com>
2022-11-27 20:22:33 +0300

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,22 @@
+class PlantsController < ApplicationController
+  wrap_parameters format: []
+   def index
+       render json: Plant.all, status: :ok
+   end
+   def show
+       plants = Plant.find_by(id:params[:id])
+       if plants
+           render json: plants, status: :ok
+       else
+           render json: {error: "plant not found"}, status: :not_found
+   end
+end
+   def create
+       plants = Plant.create(plants_params)
+       render json: plants, status: :created
+   end
+   private
+   def plants_params
+   params.permit(:name, :price, :image)
+   end
+end
